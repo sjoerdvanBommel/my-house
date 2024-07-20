8804361470c830cb3e15b4415daeba3de70574be
Sjoerd van Bommel <sjoerd.van.bommel@hotmail.com>
2024-07-20 21:10:41 +0200

    feat: add initial version of my house

--- src/components/canvas/Examples.tsx
+++ src/components/canvas/Examples.tsx
@@ -1,11 +1,10 @@
 'use client'
 
-import { useGLTF } from '@react-three/drei'
+import { Line, MeshDistortMaterial, useCursor, useGLTF } from '@react-three/drei'
 import { useFrame } from '@react-three/fiber'
-import * as THREE from 'three'
-import { useMemo, useRef, useState } from 'react'
-import { Line, useCursor, MeshDistortMaterial } from '@react-three/drei'
 import { useRouter } from 'next/navigation'
+import { useMemo, useRef, useState } from 'react'
+import * as THREE from 'three'
 
 export const Blob = ({ route = '/', ...props }) => {
   const router = useRouter()
@@ -16,7 +15,8 @@ export const Blob = ({ route = '/', ...props }) => {
       onClick={() => router.push(route)}
       onPointerOver={() => hover(true)}
       onPointerOut={() => hover(false)}
-      {...props}>
+      {...props}
+    >
       <sphereGeometry args={[1, 64, 64]} />
       <MeshDistortMaterial roughness={0.5} color={hovered ? 'hotpink' : '#1fb2f5'} />
     </mesh>
@@ -54,15 +54,8 @@ export const Logo = ({ route = '/blob', ...props }) => {
   )
 }
 
-export function Duck(props) {
-  const { scene } = useGLTF('/duck.glb')
-
-  useFrame((state, delta) => (scene.rotation.y += delta))
-
-  return <primitive object={scene} {...props} />
-}
-export function Dog(props) {
-  const { scene } = useGLTF('/dog.glb')
+export function MyHouse(props) {
+  const { scene } = useGLTF('/my-house.glb')
 
   return <primitive object={scene} {...props} />
 }
